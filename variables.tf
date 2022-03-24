02351050616d838b4aea201890f54acd5ae55451
lormadus <david.hyeshik@gmail.com>
2022-03-24 13:44:42 +0900

    updated

---
+++ variables.tf
@@ -0,0 +1,18 @@
+#
+# Variables Configuration
+#
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AKIA2Y3K6PAHG6LZ25K5"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "40d2MWsNZ0UyIAXHtRE5qX6OENE2Kz0jY2QSIjA6"
+  type        = string
+}
+
+variable "cluster-name" {
+  default = "user40-arc-eks-demo"
+  type    = string
+}
